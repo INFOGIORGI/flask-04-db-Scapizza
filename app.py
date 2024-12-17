b0815a9228f741d203982127cffba0c507a896b1
edoardo <105643778+Scapizza@users.noreply.github.com>
2024-12-17 11:48:30 +0000

    l

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST']="138.41.20.102"
+app.config['MYSQL_PORT']="53306"
+app.config['MYSQL_USER']="ospite"
+app.config['MYSQL_PASSWORD']="ospite"
+app.config['MYSQL_DB']="w3schools"
+mysql= MySQL(app)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html", titolo="Home")
 
-app.run()
+@app.route("/products")
+def products():
+    cursor=mysql.connection.cursor()
+    cursor.execute("SELECT * FROM products")
+    products=cursor.fetchall()
+    return render_template("products.html", titolo="prodotti", products=products)
+
+app.run(debug=True)
+
+
+
