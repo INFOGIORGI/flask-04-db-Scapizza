8dadb23b842928c449ed19fc5d18cf2ce570e050
edoardo <105643778+Scapizza@users.noreply.github.com>
2025-01-05 17:38:33 +0000

    g

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['MYSQL_HOST']="138.41.20.102"
-app.config['MYSQL_PORT']="53306"
+app.config['MYSQL_PORT']=53306
 app.config['MYSQL_USER']="ospite"
 app.config['MYSQL_PASSWORD']="ospite"
 app.config['MYSQL_DB']="w3schools"
@@ -17,8 +17,19 @@ def home():
 def products():
     cursor=mysql.connection.cursor()
     cursor.execute("SELECT * FROM products")
-    products=cursor.fetchall()
-    return render_template("products.html", titolo="prodotti", products=products)
+    prodotti=cursor.fetchall()
+    return render_template("products.html", titolo="prodotti", products=prodotti)
+
+@app.route("/productsPerCat/<int:category>")
+def productsPerCat(category):
+    cursor=mysql.connection.cursor()
+    cursor.execute("SELECT * FROM products")
+    prodotti=cursor.fetchall()
+    selezionati=[]
+    for p in prodotti:
+        if(p[3]==category):
+            selezionati.append(p)
+    return render_template("productsPerCat.html", titolo="prodottiPerCat", prodotti=selezionati)
 
 app.run(debug=True)
 
